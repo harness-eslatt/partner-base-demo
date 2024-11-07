4345dd68c31fd8353565ce55d4730d8ddbb56b2d
Ed Slatt <ed.slatt@gmail.com>
2024-11-07 13:31:34 -0500

    adds proxy to server

--- frontend-app/harness-webapp/server.ts
+++ frontend-app/harness-webapp/server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -17,6 +18,15 @@ export function app(): express.Express {
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
+  server.use('/backend', createProxyMiddleware({
+    target: 'http://web-backend-svc:8081',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/backend': '/'
+    },
+    logLevel: 'debug'
+  }));
+  
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
